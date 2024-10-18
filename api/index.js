e9734de772863d5128a4ba6029fdb0d8a0c0b61d
AssetGrav <417051@mail.ru>
2024-10-18 14:32:55 +0500

    add bot

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import { sendMessage } from "../telegraf";
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  const { name, phone, email, message } = req.query;
+
+  const getInfo = await sendMessage(
+    `My name ${name} I want ${type}, phone ${"https://wa.me/8" + phone}`
+  );
+  res.send(getInfo);
+};
---
+++ telegraf.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+const baseUrl = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/`;
+
+const sendMessage = async (message) => {
+  const url = `${baseUrl}sendMessage?chat_id=${process.env.CHAT_ID}&text=${message}`;
+  const response = await fetch(url);
+  return response.ok;
+};
+
+module.exports = {
+  sendMessage,
+};
